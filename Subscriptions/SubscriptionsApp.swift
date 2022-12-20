e0adf38cd5a1040afddca73a3856c23e0b6c9b9a
Paul Cristian Vasile <paul.vasile04@icloud.com>
2022-12-20 23:03:07 +0200

    Initial Commit

---
+++ Subscriptions/SubscriptionsApp.swift
@@ -0,0 +1,20 @@
+//
+//  SubscriptionsApp.swift
+//  Subscriptions
+//
+//  Created by Paul Cristian on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
